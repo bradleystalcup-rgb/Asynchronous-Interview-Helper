663b6b179c82b801d3d6e81be70244888f092e22
Brad Stalcup <bradley.stalcup@gmail.com>
2026-06-10 15:40:49 +0000

    Configure Cloudflare static deploy

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ["interview-help.evidentco.com"],
+  output: "export",
+};
 
 export default nextConfig;
